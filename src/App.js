4ce14fce4c25f34ee88a6f8920920d8e89d21679
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-02-12 19:45:29 +0100

    chore: conditional rendering, lists, keys and styling.

--- src/App.js
+++ src/App.js
@@ -9,13 +9,32 @@ import FunctionClick from "./components/FunctionClick";
 import ClassClick from "./components/ClassClick";
 import EventBind from "./components/EventBind";
 import ParentComponent from "./components/ParentComponent";
+import UserGreeting from "./components/UserGreeting";
+import NameList from "./components/NameList";
+import Stylesheet from "./components/Stylesheet";
+import Inline from "./components/Inline";
+import './css/appStyles.css';
+import styles from './css/appStyles.module.css';
 
 class App extends Component {
   render() {
     return (
       <div  className="App">
-        <ParentComponent />
+
+       <h1 className="error" >Error</h1>
+       <h1 className={styles.success} >Success</h1>
+        
         {/*
+        <Inline />
+
+        <Stylesheet primary={true}  />
+
+        <NameList />
+
+        <UserGreeting />
+
+        <ParentComponent />
+
         <EventBind />
 
         <FunctionClick />
