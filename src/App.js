1bc638781b926e1ae257cd7e1aa5a91228275c5e
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-02-12 13:18:06 +0100

    chore: setState, destructuring and event handling.

--- src/App.js
+++ src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Message from './components/Message';
+import Counter from "./components/Counter";
+import FunctionClick from "./components/FunctionClick";
+import ClassClick from "./components/ClassClick";
+import EventBind from "./components/EventBind";
+import ParentComponent from "./components/ParentComponent";
 
 class App extends Component {
   render() {
     return (
       <div  className="App">
+        <ParentComponent />
+        {/*
+        <EventBind />
+
+        <FunctionClick />
+        <ClassClick />
+
+        <Counter/>
+
         <Message />
 
-        {/*<Greet name="Bruce" heroName="Batman">
+        <Greet name="Bruce" heroName="Batman">
           <p>This is children props</p>
         </Greet>
         <Greet name="Clark" heroName="Superman">
