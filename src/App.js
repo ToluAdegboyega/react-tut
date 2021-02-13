e03a274e6763e952e3c0609bc62b913781e387e9
ToluAdegboyega <65776842+ToluAdegboyega@users.noreply.github.com>
2021-02-14 00:02:27 +0100

    chore: fragments, pure components, memo and refs.

--- src/App.js
+++ src/App.js
@@ -19,15 +19,27 @@ import Form from "./components/Form";
 import LifecycleA from "./components/LifecycleA";
 import FragmentDemo from "./components/FragmentDemo";
 import Table from "./components/Table";
+import ParentComp from "./components/ParentComp";
+import RefsDemo from "./components/RefsDemo";
+import FocusInput from "./components/FocusInput";
+import FRParentInput from "./components/FRParentInput";
 
 class App extends Component {
   render() {
     return (
       <div  className="App">
 
-        <Table />
+        <FRParentInput/>
         
         {/*
+        <FocusInput/>
+
+        <RefsDemo/>
+
+        <ParentComp />
+
+        <Table />
+
         <FragmentDemo />
 
         <LifecycleA />
